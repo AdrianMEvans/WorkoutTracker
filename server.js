888feb4d4a56f314554b6d17052068108c97ca15
Rhinoroo <73572438+AdrianMEvans@users.noreply.github.com>
2021-05-25 13:29:18 +0800

    reverted mongoose connection

--- server.js
+++ server.js
@@ -14,20 +14,14 @@ app.use(express.json());
 app.use(express.static("public"));
 
 
-// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
-  //   useNewUrlParser: true,
-  //   useFindAndModify: false,
-  //   useUnifiedTopology: true
-  // });
-  
-  app.use(require("./routes/apiRoutes.js"));
-  app.use(require("./routes/htmlRoutes.js"));
-  
-  mongoose.connect(
-    process.env.MONGODB_URI || "mongodb://localhost/workout",
-    { useNewUrlParser: true, useUnifiedTopology: true }
-  );
-  
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/workout",
+  { useNewUrlParser: true, useFindAndModify: false, useUnifiedTopology: true }
+);
+
+app.use(require("./routes/apiRoutes.js"));
+app.use(require("./routes/htmlRoutes.js"));
+
 app.listen(PORT, () => {
   console.log(`App running ===> visit at port ${PORT}!`);
 });

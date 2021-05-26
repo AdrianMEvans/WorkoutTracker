b081c37fee30128147b12d173cc77c71251b9e7a
Rhinoroo <73572438+AdrianMEvans@users.noreply.github.com>
2021-05-26 11:47:38 +0800

    reverted workout.js. moved conxion->server.js down

--- server.js
+++ server.js
@@ -13,6 +13,8 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
+app.use(require("./routes/apiRoutes.js"));
+app.use(require("./routes/htmlRoutes.js"));
 
 mongoose.connect(
   process.env.MONGODB_URI || "mongodb://localhost/workout",
@@ -22,9 +24,6 @@ mongoose.connect(
 }).catch((e) => {
   console.log('Connection Failed!!!', (e))
 });
-
-app.use(require("./routes/apiRoutes.js"));
-app.use(require("./routes/htmlRoutes.js"));
 
 app.listen(PORT, () => {
   console.log(`App running ===> visit at port ${PORT}!`);
